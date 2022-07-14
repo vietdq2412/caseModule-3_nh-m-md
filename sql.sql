a27f14b53453347884c93fed8e3b993602af4dd6
JulyHa <nguyenthingu3107@gmail.com>
2022-07-14 22:41:11 +0700

    create table data

--- sql.sql
+++ sql.sql
@@ -1,15 +1,15 @@
-create database CaseStudyMD3_QuanLySach;
+create database casestudymd3_bookmanagement;
 
-use CaseStudyMD3_QuanLySach;
+use casestudymd3_bookmanagement;
 
 create table role(
                      id int not null auto_increment primary key,
                      name varchar(10) not null
 );
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`account` (
+CREATE TABLE `casestudymd3_bookmanagement`.`account` (
                                           `id` INT NOT NULL AUTO_INCREMENT,
-                                          `fullname` NVARCHAR(50) NOT NULL,
+                                          `full_name` VARCHAR(50) NOT NULL,
                                           `username` VARCHAR(20) NOT NULL,
                                           `password` VARCHAR(15) NOT NULL,
                                           `address` VARCHAR(255) NOT NULL,
@@ -21,48 +21,46 @@ CREATE TABLE `CaseStudyMD3_QuanLySach`.`account` (
 );
 
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`category` (
+CREATE TABLE `casestudymd3_bookmanagement`.`category` (
                                            `id` INT NOT NULL AUTO_INCREMENT,
-                                           `name` NVARCHAR(45) NOT NULL,
+                                           `name` VARCHAR(45) NOT NULL,
                                            PRIMARY KEY (`id`));
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`order_status` (
+CREATE TABLE `casestudymd3_bookmanagement`.`order_status` (
                                                `id` INT NOT NULL AUTO_INCREMENT,
-                                               `name` NVARCHAR(45) NOT NULL,
+                                               `name` VARCHAR(45) NOT NULL,
                                                PRIMARY KEY (`id`));
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`author` (
+CREATE TABLE `casestudymd3_bookmanagement`.`author` (
                                          `id` INT NOT NULL AUTO_INCREMENT,
-                                         `name` NVARCHAR(30) NOT NULL,
+                                         `name` VARCHAR(30) NOT NULL,
                                          `date_of_birth` DATE NOT NULL,
-                                         `number_of_arts` INT(10) NOT NULL,
-                                         `nation` VARCHAR(45) NOT NULL,
+                                         `number_of_arts` INT(10) NOT NULL, -- số tác phẩm của tác giả
+                                         `nation` VARCHAR(45) NOT NULL, -- quốc tịch
                                          `image` VARCHAR(255) NOT NULL,
                                          PRIMARY KEY (`id`));
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`book` (
+CREATE TABLE `casestudymd3_bookmanagement`.`book` (
                                        `id` INT NOT NULL AUTO_INCREMENT,
-                                       `title` NVARCHAR(45) NOT NULL,
-                                       `column` INT(10) NOT NULL,
+                                       `title` VARCHAR(45) NOT NULL, -- tên sách
                                        `category_id` INT(10) NOT NULL,
                                        `author_id` INT(10) NOT NULL,
-                                       `publish_year` INT(10) NOT NULL,
-                                       `description` LONGTEXT NOT NULL,
+                                       `publish_year` INT(10) NOT NULL, -- năm xuất bản
+                                       `description` LONGTEXT NOT NULL, -- mô tả
                                        `image` VARCHAR(255) NOT NULL,
-                                       `view` INT(10) NOT NULL,
+                                       `views` INT(10) NOT NULL,
                                        `quantity` INT(10) NOT NULL,
                                        `price` DOUBLE NOT NULL,
                                        PRIMARY KEY (`id`));
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`order` (
+CREATE TABLE `casestudymd3_bookmanagement`.`order` (
                                         `id` INT NOT NULL AUTO_INCREMENT,
                                         `create_time` DATE NOT NULL,
-                                        `total_price` DOUBLE NOT NULL,
                                         `account_id` INT(10) NOT NULL,
                                         `order_status_id` INT(10) NOT NULL,
                                         PRIMARY KEY (`id`));
 
-CREATE TABLE `CaseStudyMD3_QuanLySach`.`order_detail` (
+CREATE TABLE `casestudymd3_bookmanagement`.`order_detail` (
                                                `id` INT(10) NOT NULL AUTO_INCREMENT,
                                                `quantity` INT(10) NOT NULL,
                                                `order_id` INT(10) NOT NULL,
@@ -77,17 +75,17 @@ ALTER TABLE book ADD CONSTRAINT id FOREIGN KEY (author_id)
 ALTER TABLE book ADD CONSTRAINT id_category FOREIGN KEY (category_id)
     REFERENCES category(id);
 
-ALTER TABLE `CaseStudyMD3_QuanLySach`.`order` ADD CONSTRAINT order_orderstatus FOREIGN KEY (order_status_id)
+ALTER TABLE `casestudymd3_bookmanagement`.`order` ADD CONSTRAINT order_orderstatus FOREIGN KEY (order_status_id)
     REFERENCES order_status(id);
 
-ALTER TABLE `CaseStudyMD3_QuanLySach`.`order` ADD CONSTRAINT order_account FOREIGN KEY (account_id)
+ALTER TABLE `casestudymd3_bookmanagement`.`order` ADD CONSTRAINT order_account FOREIGN KEY (account_id)
     REFERENCES account(id);
 
-ALTER TABLE `CaseStudyMD3_QuanLySach`.`order_detail` ADD CONSTRAINT orderdetail_order FOREIGN KEY (order_id)
-    REFERENCES `CaseStudyMD3_QuanLySach`.`order`(id);
+ALTER TABLE `casestudymd3_bookmanagement`.`order_detail` ADD CONSTRAINT orderdetail_order FOREIGN KEY (order_id)
+    REFERENCES `casestudymd3_bookmanagement`.`order`(id);
 
-ALTER TABLE `CaseStudyMD3_QuanLySach`.`order_detail` ADD CONSTRAINT orderdetail_book FOREIGN KEY (book_id)
-    REFERENCES `CaseStudyMD3_QuanLySach`.`book`(id);
+ALTER TABLE `casestudymd3_bookmanagement`.`order_detail` ADD CONSTRAINT orderdetail_book FOREIGN KEY (book_id)
+    REFERENCES `casestudymd3_bookmanagement`.`book`(id);
 
 
 -- end--------------------
