d4124472aaf5d4b4dfa36bea173a2eba2fcc7ff4
vietdq2412 <67895234+vietdq2412@users.noreply.github.com>
2022-07-17 05:42:22 +0700

    list category api, merge code

---
+++ src/main/java/com/codegym/case_module3/service/author/IAuthorService.java
@@ -0,0 +1,7 @@
+package com.codegym.case_module3.service.author;
+
+import com.codegym.case_module3.model.Author;
+import com.codegym.case_module3.service.IService;
+
+public interface IAuthorService extends IService<Author> {
+}
